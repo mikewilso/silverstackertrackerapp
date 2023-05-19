c7d3ef56d193c149d48fe412041e6b5e67c9d2a6
Michael Wilson <mikewilsooo@gmail.com>
2023-05-19 14:14:34 -0600

    removed import breaking startup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css';
 // import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AppContainer } from './components/AppContainer';
