4c107c5b5278be5d561a88ef728036536e634571
Michael Goss Wilson <mikewilsooo@gmail.com>
2024-05-24 20:31:15 -0600

    Added form validation and ghost text placeholders

--- client/src/components/views/AddItem.tsx
+++ client/src/components/views/AddItem.tsx
@@ -25,7 +25,7 @@ interface formData {
     purchasedate: string;
     purchasedfrom: string;
     purchaseprice: number;
-    shape: string;
+    form: string;
     mint: string;
     metaltype: string;
     purity: number;
@@ -111,59 +111,104 @@ export const AddItem = () => {
                 form={form}
                 onFinish={onFinish}
             >
-                <Form.Item label='Name of Item' name='name'>
-                    <Input />
+                <Form.Item 
+                    label='Name of Item' 
+                    name='name'
+                    rules={[{ required: true, message: 'Please input the name of the item!' }]}
+                >
+                    <Input placeholder='Enter item name'/>
                 </Form.Item>
-                <Form.Item label='Description' name='description'>
-                    <Input />
+                <Form.Item 
+                    label='Description' 
+                    name='description'
+                    rules={[{ required: false, message: 'Please input the description!' }]}
+                >
+                    <Input placeholder="Enter item description (optional)" />
                 </Form.Item>
-                <Form.Item label='Purchase Date' name='purchasedate'>
+                <Form.Item 
+                    label='Purchase Date' 
+                    name='purchasedate'
+                    rules={[{ required: true, message: 'Please select the purchase date!' }]}
+                >
                     <DatePicker />
                 </Form.Item>
-                <Form.Item label='Purchased From' name='purchasedfrom'>
-                    <Input />
+                <Form.Item 
+                    label='Purchased From' 
+                    name='purchasedfrom'
+                    rules={[{ required: true, message: 'Please input where it was purchased from!' }]}
+                >
+                    <Input placeholder='Enter place of purchase(coin shop, APMEX, etc.)'/>
                 </Form.Item>
-                <Form.Item label='Purchase Price' name='purchaseprice'>
-                    <InputNumber />
+                <Form.Item 
+                    label='Purchase Price' 
+                    name='purchaseprice'
+                    rules={[{ required: true, message: 'Please input the purchase price!' }]}
+                >
+                    <InputNumber min={0} formatter={value => `$ ${value}`} style={{ width: '25%' }}/>
                 </Form.Item>
-                <Form.Item label='Shape' name='shape'>
-                    <Input />
+                <Form.Item 
+                    label='Form' 
+                    name='form' 
+                    rules={[{ required: true, message: 'Please input the form of the metal!' }]}
+                >
+                    <Input placeholder='Enter form of the item'/>
                 </Form.Item>
-                <Form.Item label='Mint' name='mint'>
-                    <Input />
+                <Form.Item 
+                    label='Mint' 
+                    name='mint'
+                    rules={[{ required: true, message: 'Please input the maker of this item!' }]}
+                >
+                    <Input placeholder='Enter maker of the item'/>
                 </Form.Item>
-                <Form.Item label='Metal Type' name='metaltype'>
-                    <Select>
+                <Form.Item 
+                    label='Metal Type' 
+                    name='metaltype'
+                    rules={[{ required: true, message: 'Please choose the precious metal type!' }]}
+                >
+                    <Select placeholder='Enter metal type'>
                         {metals.map((metal)=>(
                             <Select.Option value={metal.metalvalue}>{metal.metaltype}</Select.Option>
                         ))}
                     </Select>
                 </Form.Item>
-                <Form.Item label='Weight' name='unitweight'>
-                    <InputNumber />
+                <Form.Item 
+                    label='Weight' 
+                    name='unitweight'
+                    rules={[{ required: true, message: 'Please input the weight!' }]}>
+                    <InputNumber min={0.01} placeholder='0.01'/>
                 </Form.Item>
-                <Form.Item label='Unit of Weight' name='weighttype'>
+                <Form.Item 
+                    label='Unit of Weight' 
+                    name='weighttype'
+                    rules={[{ required: true, message: 'Please choose the unit of weight!' }]}>
                     <Radio.Group>
                         <Radio value='ozt'>Troy Ounces</Radio>
                         <Radio value='oz'>Ounces</Radio>
                         <Radio value='grams'>Grams</Radio>
                     </Radio.Group>
                 </Form.Item>
-                <Form.Item label='Purity' name='purity'>                    
+                <Form.Item 
+                    label='Purity' 
+                    name='purity'
+                    rules={[{ required: true, message: 'Please input the purchase price!' }]}>                    
                     <Select>
                         <Select.Option value='1'>Pure(.999)</Select.Option>
-                        <Select.Option value='.925'>Sterling(.925)</Select.Option>
-                        <Select.Option value='.9'>Coin(90%)</Select.Option>
-                        <Select.Option value='.958'>Britannia(95.8%)</Select.Option>
-                        <Select.Option value='.8'>Sterling(.800)</Select.Option>
+                        <Select.Option value='0.925'>Sterling(.925)</Select.Option>
+                        <Select.Option value='0.9'>Coin(90%)</Select.Option>
+                        <Select.Option value='0.958'>Britannia(95.8%)</Select.Option>
+                        <Select.Option value='0.8'>Sterling(.800)</Select.Option>
+                        <Select.Option value='0.35'>War Nickel(35%)</Select.Option>
                     </Select>
                 </Form.Item>
-                <Form.Item label='Amount' name='amount'>
-                    <InputNumber />
+                <Form.Item 
+                    label='Amount' 
+                    name='amount'
+                    rules={[{ required: true, message: 'Please input the amount!' }]}>
+                    <InputNumber min={1} placeholder='0'/>
                 </Form.Item>
                 <Form.Item style={{ textAlign: 'center', marginLeft: '100px' }}>
                     <Button htmlType='submit' style={{ width: '100%' }}>
-                        Submit
+                        Add to the Stack!
                     </Button>
                 </Form.Item>
             </Form>
