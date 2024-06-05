475c468962f20a6289f4680867d1fd9c7d1a8667
Michael Goss Wilson <mikewilsooo@gmail.com>
2024-06-04 23:01:12 -0600

    fixed fetchMints tests

--- client/src/components/hooks/useFetchMints.test.ts
+++ client/src/components/hooks/useFetchMints.test.ts
@@ -1,25 +1,25 @@
 import { renderHook } from '@testing-library/react-hooks';
 import axios from 'axios';
-import { useFetchPurchasePlaces } from './useFetchPurchasePlaces';
+import { useFetchMints } from './useFetchMints';
 
 jest.mock('axios');
 
-describe('useFetchPurchasePlaces', () => {
-  it('fetches data successfully from an API', async () => {
-    const data = [{ purchasedfrom: 'Test place' }];
+describe('useFetchMints', () => {
+  it('fetches successfully data from an API', async () => {
+    const data = [{ mint: 'Test mint' }];
     (axios.get as jest.Mock).mockResolvedValueOnce({ data });
 
-    const { result, waitForNextUpdate } = renderHook(() => useFetchPurchasePlaces());
+    const { result, waitForNextUpdate } = renderHook(() => useFetchMints());
 
     await waitForNextUpdate();
 
-    expect(result.current).toEqual([{ value: 'Test place' }]);
+    expect(result.current).toEqual([{ value: 'Test mint' }]);
   });
 
   it('handles error', async () => {
     (axios.get as jest.Mock).mockRejectedValueOnce(new Error('Error fetching data'));
 
-    const { result, waitForNextUpdate } = renderHook(() => useFetchPurchasePlaces());
+    const { result, waitForNextUpdate } = renderHook(() => useFetchMints());
 
     await waitForNextUpdate();
 
